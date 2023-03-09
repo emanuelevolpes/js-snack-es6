f61bfdb8e0d55e99c2456b1c27b3e7e0b95b001f
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-09 16:12:28 +0100

    snack4 team array

---
+++ snack4/js/main.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/*SNACK 4
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+BONUS
+Stampare in pagina oltre che in console!*/
+
+const team = [
+    {
+        nome: 'Atalanta',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Bologna',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Cremonese',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Empoli',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Fiorentina',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Hellas Verona',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Inter',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Juventus',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Lazio',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Lecce',
+        'punti fatti': 0,
+        'falli subiti': 0
+    }
+];
+
+console.log(team);
