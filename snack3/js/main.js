232280d999d57a5e199ae9681a60fb18fcaa234b
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-09 15:10:49 +0100

    bike list

---
+++ snack3/js/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/*SNACK 3
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare in console la bici con peso minore utilizzando destructuring e template literal
+BONUS
+Stampare in pagina oltre che in console!*/
+
+const bike = [
+    {
+        nome: 'Cannondale',
+        peso: 12
+    },
+    {
+        nome: 'Factor',
+        peso: 10
+    },
+    {
+        nome: 'Orbea',
+        peso: 8
+    },
+    {
+        nome: 'Pinarello.',
+        peso: 11
+    },
+    {
+        nome: 'Scott.',
+        peso: 7
+    },
+    {
+        nome: 'Specialized.',
+        peso: 6.8
+    }
+];
+
+console.log(bike);
